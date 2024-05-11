61e1baa9ca9d3394f11132f6306689c8e9bc0f93
PrathmTayade <122280972+PrathmTayade@users.noreply.github.com>
2024-05-11 19:12:10 +0530

    frontend init

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,4 @@
 import { connect, disconnect } from "mongoose";
-import { projects, clients } from "../sampleData.js";
 import Project from "../models/Project.js";
 import Client from "../models/Client.js";
 
@@ -8,32 +7,32 @@ const connectDB = async () => {
   console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
 
-export const addToDb = async () => {
-  try {
-    // Check if projects collection is empty
-    // const projectCount = await Project.countDocuments();
-    // if (projectCount === 0) {
-    //   // Insert projects data if collection is empty
-    //   await Project.insertMany(projects);
-    //   console.log("Projects data inserted successfully");
-    // } else {
-    //   console.log("Projects data already exists");
-    // }
+// export const addToDb = async () => {
+//   try {
+//     // Check if projects collection is empty
+//     // const projectCount = await Project.countDocuments();
+//     // if (projectCount === 0) {
+//     //   // Insert projects data if collection is empty
+//     //   await Project.insertMany(projects);
+//     //   console.log("Projects data inserted successfully");
+//     // } else {
+//     //   console.log("Projects data already exists");
+//     // }
 
-    // Check if clients collection is empty
-    const clientCount = await Client.countDocuments();
-    if (clientCount === 0) {
-      // Insert clients data if collection is empty
-      await Client.insertMany(clients);
-      console.log("Clients data inserted successfully");
-    } else {
-      console.log("Clients data already exists");
-    }
-  } catch (err) {
-    console.error("Error inserting data:", err);
-  } finally {
-    disconnect();
-  }
-};
+//     // Check if clients collection is empty
+//     const clientCount = await Client.countDocuments();
+//     if (clientCount === 0) {
+//       // Insert clients data if collection is empty
+//       await Client.insertMany(clients);
+//       console.log("Clients data inserted successfully");
+//     } else {
+//       console.log("Clients data already exists");
+//     }
+//   } catch (err) {
+//     console.error("Error inserting data:", err);
+//   } finally {
+//     disconnect();
+//   }
+// };
 
 export default connectDB;
